1fa3c400e79b9d9049fc3fc4403b4d046b66ad47
agustin.delrio <agustin.delrio@retcon.com.ar>
2020-04-08 17:27:29 -0300

    create Mamiland proyec

---
+++ src/main/java/com/mamiland/backend/MamilandApplication.java
@@ -0,0 +1,13 @@
+package com.mamiland.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MamilandApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MamilandApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mamiland/backend/MamilandApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mamiland.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MamilandApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
